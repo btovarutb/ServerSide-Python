41f093df5373db481b78af149ed90f6ad887d9ee
btovarutb <111324906+btovarutb@users.noreply.github.com>
2022-08-15 19:56:14 -0500

    Update app.py

--- ServerSide-Python-main/web/app.py
+++ ServerSide-Python-main/web/app.py
@@ -34,5 +34,47 @@ def people():
     return render_template('people.html', value=data)
 
 
+@app.route('/people_update/<id_person>', methods=['GET'])
+def pet_update(id_person):
+    return render_template('person_update.html', value=id_person)
+
+
+@app.route('/person_update_detail', methods=['POST'])
+def person_update_detail():
+    id = request.form['id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    status = request.form['status']
+    update_data = {
+                        "id": id,
+                        "category": {
+                            "id": 0,
+                            "first_name": "string",
+                            "last_name": "string"
+                        },
+                        "first_name": first_name,
+                        "last_name": last_name,
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 0,
+                                "first_name": "string",
+                                "last_name": "string"
+                            }
+                        ],
+                        "status": status
+                    }
+    response = requests.put(api_url_update, json=update_data)
+    return render_template('person_detail.html', value=(id, name, status))
+
+
+@app.route('/person_delete/<id_person>', methods=['GET'])
+def pet_delete(id_person):
+    api_url = "".format(id_person)
+    response = requests.delete(api_url)
+    return render_template('person_detail.html', value="Delete successfully")
+
 if __name__ == '__main__':
     app.run()
